49c7747fb71e260ed71313d7018818e67811b184
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-03-16 00:11:07 -0300

    Create component Profile

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá</Text>
+
+          <Text style={styles.username}>Rodrigo</Text>
+        </View>
+        <Text style={styles.message}>Hoje é dia de vitória</Text>
+      </View>
+    </View>
+  );
+}
