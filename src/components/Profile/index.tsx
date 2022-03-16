9643ad2708b68e805d0c84186a50ce3737ce1989
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-03-16 00:25:49 -0300

    Styles Avatar

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
   return (
     <View style={styles.container}>
+      <Avatar urlImage="https://github.com/rodrigosaantos.png" />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
